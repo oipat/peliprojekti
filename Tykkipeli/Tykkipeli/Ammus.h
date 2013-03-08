09e9afa152b49914d79487c63c291b8c850c4d8f
unknown <tapioku@VW-VISUAL-12.pk.amk.root>
2013-03-08 12:49:39 +0200

    kansiorakenne

---
+++ Tykkipeli/Tykkipeli/Ammus.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Ammus
+{
+			
+private:
+	double paino;
+    double voima_x;
+    double voima_y;
+    double voima_g;
+    int pos_x, pos_y, ppos_x, ppos_y;
+	
+public:
+	Ammus(double voima_x,double voima_y,int x,int y);
+	~Ammus(void);
+	
+	
+	
+	
+	
+};
