f89cb24397af595906a4a05ca3f559f887e5a43a
Mahox <mahox7@gmail.com>
2013-03-10 19:06:04 +0200

    Ammus ja Testaus luokkaa laajennettu

--- Ammus.h
+++ Ammus.h
@@ -17,13 +17,16 @@ public:
 	void setVoima_x(double v) {voima_x=v;}
 	void setVoima_y(double v) {voima_y=v;}
 	void setVoima_g(double v) {voima_g=v;}
-	
-
 	double getVoima_x() const {return voima_x;}
 	double getVoima_y() const {return voima_y;}
 	double getVoima_g() const {return voima_g;}
-
-	
+	int getPos_x() const {return pos_x;}
+	int getPos_y() const {return pos_y;}
+	int getPPos_x() const {return ppos_x;}
+	int getPPos_y() const {return ppos_y;}
+	void nextX(long time);
+	void nextY(long time);
+	void stop();
 	
 	
 	
--- Testaus.cpp
+++ Testaus.cpp
@@ -5,10 +5,20 @@ using namespace std;
 int main(void)
 {
 
-	Ammus *ammus= new Ammus(52.0,12.0,12,535);
-	cout << "trololol" <<ammus->getVoima_x() <<endl;
-	cout << "voima y" <<ammus->getVoima_y() <<endl;
+	Ammus *ammus= new Ammus(52.0,42.0,12,535);
+	cout << "voima x: " <<ammus->getVoima_x() <<endl;
+	cout << "voima y: " <<ammus->getVoima_y() <<endl;
+	time_t msec = time(0) * 1000;
+	cout << "pos x: " <<ammus->getPos_x() <<endl;
+	msec = msec - time(0) * 1000;
+	ammus->nextX(msec+100);
+	cout << "pos x 2: " <<ammus->getPos_x() <<endl;
+	cout << "ppos x: " <<ammus->getPPos_x() <<endl;
+
+	cout << "pos y: " <<ammus->getPos_y() <<endl;
 	
+	ammus->nextY(10);
+	cout << "pos y 2: " <<ammus->getPos_y() <<endl;
 	cin.get();
 	return 0;
 }
