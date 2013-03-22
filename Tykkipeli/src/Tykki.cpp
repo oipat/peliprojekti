4bb2bc660dd2d934340c1daedcf351147c20b0d7
unknown <niklasks@VW-VISUAL-10.pk.amk.root>
2013-03-22 14:24:30 +0200

    Lisaa roskaa

---
+++ Tykkipeli/src/Tykki.cpp
@@ -0,0 +1,17 @@
+#include "Tykki.h"
+#include "stdafx.h"
+using namespace std;
+
+
+Tykki::Tykki(double health,double reloadAika)
+{
+
+}
+
+Tykki::Tykki()
+{
+}
+
+Tykki::~Tykki(void)
+{
+}
---
+++ Tykkipeli/src/Tykki.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Tykki
+{
+private:
+	double health;
+	double reloadAika;
+
+
+
+public:
+	Tykki();
+	~Tykki(void);
+	Tykki(double h,double r);
+	
+
+};
